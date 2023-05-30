9384fea7b855316cf6ad297c18cbbb7d09f80389
asamirkhan <134246137+asamirkhan@users.noreply.github.com>
2023-05-30 12:38:28 +0600

    Add files via upload

---
+++ config/asgi.py
@@ -0,0 +1,18 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+
+django_asgi_app = get_asgi_application()
+
+from publications import routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        ),
+    }
+)
